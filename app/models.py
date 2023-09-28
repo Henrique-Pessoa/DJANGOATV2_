0b67926d86da0bbb20b7ca34f17442e35433cc7a
Eduardo-Hoffmann <123705923+Eduardo-Hoffmann@users.noreply.github.com>
2023-09-27 22:15:14 -0300

    -a

--- app/models.py
+++ app/models.py
@@ -12,38 +12,62 @@ options = (
 )
 
 
-class Fotos():
-    link = models.CharField(max_length=2000,  blank=False, null=False),
+class Fotos(models.Model):
+    link = models.CharField(max_length=2000,  blank=False, null=False,default="")
+
+    def __str__(self):
+        return self.link
 
 class Categorias(models.Model):
     categoria = models.CharField(max_length=200, blank=False,null=False),
 
+    def __str__(self):
+        return self.categoria
+
 class Viagens(models.Model):
     titulo = models.CharField(max_length=200, blank=False, null=False),
-    descricao = models.CharField(max_length=2000,blank=False, null=False),
-    endereco = models.CharField(max_length=200, blank=False, null=False),
-    cidade = models.CharField(max_length=200, blank=False,null=False),
+    descricao = models.CharField(max_length=2000,blank=False, null=False, ),
+    endereco = models.CharField(max_length=200, blank=False, null=False,),
+    cidade = models.CharField(max_length=200, blank=False,null=False,),
     dataInicial = models.DateTimeField(default=timezone.now,blank=False,null=False),
     dataFinal = models.DateTimeField(default=timezone.now,blank=False,null=False),
-    fotos = models.ForeignKey(Fotos, null=False, blank=False)
-    valorDiaria = models.DecimalField(blank=False,null=False,nullable=False,decimal_places=2)
-    categoria = models.ForeignKey(Categoria, null=False, blank=False)
+    fotos = models.ForeignKey(Fotos, null=False, blank=False, on_delete=models.CASCADE)
+    valorDiaria = models.DecimalField(blank=False,null=False,decimal_places=2,max_digits=4)
+    categoria = models.ForeignKey(Categorias, null=False, blank=False, related_name='categorasdjawdç', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
 
 class Pagamentos(models.Model):
-    usuario = models.ForeignKeyField(User,blank=False,null=False)
     numeroCartao = models.CharField(max_length=20)
     numeroBoleto = models.CharField(max_length=2000)
     codigoPix = models.CharField(max_length=2000)
     status = models.IntegerField(choices=options,default=1)
     
+    def __str__(self):
+        return self.status
+    
 class Reserva(models.Model):
-    usuario = models.ForeignKey(User, blank=False, null=False,related_name="usurioReserva")
-    viagem = models.ForeignKey(Viagens,blank=False,null=False,related_name="viagemReseva")
+    usuario = models.ForeignKey(User, blank=False, null=False,related_name="usurioReserva" , on_delete=models.CASCADE)
+    viagem = models.ForeignKey(Viagens,blank=False,null=False,related_name="viagemReseva",  on_delete=models.CASCADE)
     dataReserva = models.DateTimeField(),
-    valorDiaria = models.ForeignKey(Viagens, blank=False, null=False,related_name="diariaViagem")
+    valorDiaria = models.ForeignKey(Viagens, blank=False, null=False,related_name="diariaViagem",  on_delete=models.CASCADE)
     dias = models.IntegerField(blank=False, null=False),
-    valorFinal = models.DecimalField(null=False,blank=False,decimal_places=2)
+    valorFinal = models.DecimalField(null=False,blank=False,max_digits=2,decimal_places=1)
     animal = models.BooleanField(default=False)
     nota = models.DecimalField(null=False, blank=False,max_digits=2,decimal_places=1,validators=[MinValueValidator(0),MaxValueValidator(10)])
-    pagamentoId = models.ForeignKey(Pagamento, blank=False, related_name="pagamentoId")
+    pagamentoId = models.ForeignKey(Pagamentos, blank=False, related_name="pagamentoId",on_delete=models.CASCADE)
     
+    def __save__(self, *args, **kwargs):
+        self.valorFinal = self.viagem__valorDiaria * self.dias
+        super(Reserva, self).__save__(*args, **kwargs)
+    
+    def __str__(self):
+        return self.usuario
+    
+class Auxiliar(models.Model):
+    viagem = models.ForeignKey(Viagens, blank=False, related_name="viagemAuxiliar",on_delete= models.CASCADE)
+    status = models.BooleanField(default=False,null=False, blank=False)
+    dia = models.DateField(null=False, blank=False,default=timezone.now)
+    def __str__(self):
+        return self.usuario
--- app/views.py
+++ app/views.py
@@ -1,11 +1,102 @@
 from django.shortcuts import render
 from .models import *
 from .serializers import *
+from rest_framework import viewsets,status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser,BasePermission,IsAuthenticated
+from rest_framework.decorators import permission_classes
+
+
+
+class CustomUserPermission1(BasePermission):
+    def has_permission(self, request, view):
+        if request.method == 'POST':
+            return True
+        elif request.method == 'PUT':
+            return True
+        elif request.method == 'DELETE':
+            return True
+        return request.user and request.user.IsAdminUser
 
 class ViagemView():
+    permission_classes = [CustomUserPermission1]
+    
     def get(self, request):
         serializer = ViagensSerializer(Viagens.objects.all(), many=True)
         return Response(status=200, data=serializer.data)
     
+    def post(self, request):
+        serializer = ViagensSerializer(data=request.data)
+        if serializer.is_valid():
+            viagens = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST,data = serializer.errors)
+    
+    def put(self, request, viagemId = ''):
+        viagensFound = ViagensSerializer.objects.get(id=viagemId)
+        viagensJson = request.data 
+        viagensSerialized = ViagensSerializer(viagensFound, data=viagensJson)
+        if viagensSerialized.is_valid():
+            viagensSerialized.save()
+            return Response(status=status.HTTP_200_OK, data=viagensSerialized.data)
+        return Response(status= status.HTTP_400_BAD_REQUEST, data = serializers.errors)
+    
+    def delete(self, request, viagemId = ''):
+        viagensFound = ViagensSerializer.objects.get(id=viagemId)
+        try:
+            viagensFound.delete()
+            return Response(status=status.HTTP_200_OK,data="Usuario deletado")
+        except:
+            return Response(status=status.HTTP_204_NO_CONTENT,data=serializers.errors)
+
+class CategoriasView(models.Model):
+    
+    def get(self, request):
+        serializer = CategoriasSerializer(Categorias.objects.all(), many=True)
+        return Response(status=200, data=serializer.data)
+    
+    def post(self, request):
+        serializers = CategoriasSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST,data=serializers.errors)
+    
+    def delete(self, request,categoriaId = ''):
+        categoriaFound = CategoriasSerializer.get(categoriaId=categoriaId)
+        try:
+            categoriaFound.delete()
+            return Response(data="Usuario apagado",status=status.HTTP_200_OK)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST,data=serializers.erro)
+        
+    def put(self, request,categoriaId = ''):
+        categoriaFound = CategoriasSerializer.get(categoriaId=categoriaId)
+        categoriaSerialized = CategoriasSerializer.get(categoriaFound,data=request.data)
+        if categoriaSerialized.is_valid():
+            categoriaSerialized.save()
+            return Response(data="Usuario atualizado",status=status.HTTP_200_OK)
+        return Response(data="Usuario atualizado",status=status.HTTP_400_BAD_REQUEST,data=categoriaSerialized.errros)
+
+
+class ReservaView(viewsets.ModelViewSet):
+    queryset = Reserva.objects.all()
+    serializer_class = ReservaSerializers
+    permission_classes = (IsAuthenticated,)
+    
+class PagamentosView(viewsets.ModelViewSet):
+    queryset = Pagamentos.objects.all()
+    serializer_class = PagamentosSerializers
+    permission_classes = (IsAuthenticated,)
+    
+
+class FotosView(viewsets.ModelViewSet):
+    queryset = Pagamentos.objects.all()
+    serializer_class = PagamentosSerializers
+    permission_classes = (IsAuthenticated,)
+    
+class Auxiliar(viewsets.ModelViewSet):
+    queryset = Pagamentos.objects.all()
+    serializer_class = PagamentosSerializers
+    permission_classes = (IsAdminUser,)
